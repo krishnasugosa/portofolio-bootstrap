26ee546d5ee76096771527893a00514782a80b58
code.shreek <127308616+krishnasugosa@users.noreply.github.com>
2024-08-13 20:05:23 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+//game
+const karakter = document.getElementById('karakter');
+const btn1 = document.getElementById('right');
+const btn2 = document.getElementById('left');
+
+let left = 0;
+
+btn1.addEventListener('click',()=>{
+    left-=5;
+    karakter.style.left = left + 'px';
+    karakter.style.backgroundImage = 'url(skull4.png)';
+    ResetKarakter1();
+});
+
+function ResetKarakter1(){
+    setTimeout(() => {
+     karakter.style.backgroundImage = 'url(skull3.png)';
+    }, 100);
+ };
+
+btn2.addEventListener('click',()=>{
+    left+=5;
+    karakter.style.left = left + 'px';
+    karakter.style.backgroundImage = 'url(skull2.png)';
+    ResetKarakter2();
+});
+
+function ResetKarakter2(){
+   setTimeout(() => {
+    karakter.style.backgroundImage = 'url(skull1.png)';
+   }, 100);
+};
+
+//toggle navbar
+const OpenBtn = document.getElementById('Openbtn');
+const Closebtn = document.getElementById('Closebtn');
+const navbar = document.querySelector('.nav');
+const NavLinks1 = document.querySelector('.nav-links1');
+const NavLinks2 = document.querySelector('.nav-links2');
+const NavLinks3 = document.querySelector('.nav-links3');
+OpenBtn.addEventListener('click',()=>{
+    navbar.classList.add('show');
+});
+Closebtn.addEventListener('click',()=>{
+    navbar.classList.remove('show');
+});
+
+NavLinks1.addEventListener('click',()=>{
+    navbar.classList.remove('show');
+});
+
+NavLinks2.addEventListener('click',()=>{
+    navbar.classList.remove('show');
+});
+
+NavLinks3.addEventListener('click',()=>{
+    navbar.classList.remove('show');
+});
